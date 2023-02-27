ec06c220229cf252791164f2cfbfbe6be03d83e0
Eslam Halawany <halawany.me@gmail.com>
2023-02-27 13:36:11 +0200

    created language project, started new app called languagesAPI, created models

---
+++ languageAPI/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Paradigm(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+    creator = models.CharField(max_length=200)
+    created_at = models.DateField()
+    paradigm = models.ForeignKey(Paradigm, on_delete=models.CASCADE)
+    description = models.TextField()
