05b8af7554f90b1846df51d24521bf531e2d781d
Eslam Halawany <halawany.me@gmail.com>
2023-03-01 14:41:48 +0200

    Added rest_framework urls

--- languageAPI/views.py
+++ languageAPI/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 from .serializers import LanguageSerializer, ParadigmSerializer
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Language, Paradigm
 
 class LanguageList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated,]
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
 
