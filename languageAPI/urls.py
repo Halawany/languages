987818b8f92b432f2b113a67deb178cafa230df7
Eslam Halawany <halawany.me@gmail.com>
2023-03-20 14:57:16 +0200

    Added new feature where user can view every language detail in new page

--- languageAPI/urls.py
+++ languageAPI/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import LanguageList, ParadigmsList
+from .views import LanguageList, ParadigmsList, LanguageUpdateOrDestroy
 
 urlpatterns = [
     path('', LanguageList.as_view(),),
     path('paradigms/', ParadigmsList.as_view(),),
+    path('language/<int:pk>', LanguageUpdateOrDestroy.as_view(), ),
 ]
