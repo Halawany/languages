ff6a8b6a8e2ac733cf93d2d8cc52970ec1a2085f
Eslam Halawany <halawany.me@gmail.com>
2023-09-13 02:57:32 +0300

    Removed api from url

--- languages/urls.py
+++ languages/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('languageAPI.urls')),
+    path('', include('languageAPI.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
